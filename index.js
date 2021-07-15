93d92e1d6b7a3ea6471b6851665591c4ed64a1dc
unknown <bithi232@gmail.com>
2021-07-15 21:31:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+const data = require('./create json data.json');
+//console.log(data);
+//Access:
+/** 
+console.log(data.friends[0].name);
+*/
+//Modify:
+//console.log(data.friends[0].name="Bijoy");
+//Delete:
+
+delete data.friends[0].name;
+console.log(data);
+
+
